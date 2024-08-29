05cbcd32594761b9a500dc59ec8f57a94a7f0ace
joelthiek <joelrokiemlothiek@gmail.com>
2024-08-29 19:03:35 +0530

    added features on file click

---
+++ chatbox/chatbot.js
@@ -0,0 +1,4 @@
+document.getElementById('attach_button').addEventListener('click', function() {
+    // Trigger the hidden file input click event
+    document.getElementById('file_input').click();
+});
